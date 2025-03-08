38b5d9a4da0bbe0ff6347eb951cb07ccebc162de
Andreas Grønbeck <andreas@Andreass-MacBook-Air.local>
2025-03-08 18:51:36 +0100

    Added test objects

---
+++ bdtourist.py
@@ -0,0 +1,5 @@
+#The Boredless Tourist
+
+destinations = ["Paris,France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
